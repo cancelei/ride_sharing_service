add7f0ade2de4537b8e71c92f14076e77dc79a59
cancelei <glauber.bannwart@gmail.com>
2025-01-14 09:44:13 -0600

    Revert "feat/update views and other configs"
    This reverts commit 594ff7b9b3ef838173c3d432d7aa33b57d8df6e3.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,1 +1,38 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [ :create ]
+  before_action :configure_account_update_params, only: [ :update ]
 
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :phone,
+      :role,
+      { notification_preferences: {} },
+      :whatsapp_number,
+      :telegram_username
+    ])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :phone,
+      { notification_preferences: {} },
+      :whatsapp_number,
+      :telegram_username
+    ])
+  end
+
+  def after_sign_up_path_for(resource)
+    case resource.role
+    when "passenger"
+      new_passenger_profile_path
+    when "driver"
+      new_driver_profile_path
+    when "cab_association"
+      new_cab_association_path
+    else
+      root_path
+    end
+  end
+end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,1 +1,7 @@
+class Users::SessionsController < Devise::SessionsController
+  protected
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || super
+  end
+end
